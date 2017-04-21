4fce367762f18979285f038778cc452c40ceff52
Michael Axelson <axelmichael11@gmail.com>
2017-04-20 21:09:55 -0700

    almost done with event handler... clicks are weird

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
 'use strict';
 //constructor function to log image information
 
-// var imageArray= ['img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg','img/bubblegum.jpg','img/chair.jpg','img/cthulhu.jpg','img/dog-duck.jpg','img/dragon.jpg','img/pen.jpg','img/pet-sweep.jpg','img/scissors.jpg','img/shark.jpg','img/sweep.png','img/tauntaun.jpg','img/unicorn.jpg','img/usb.gif','img/water-can.jpg','img/wine-glass.jpg'];
-
-// yeah i don't even need the constructor to generate pictures... something for resubmit!
-
-//**************
-// var imageArray = [
-//   new Image('img/','bag',''),
-//   new Image('img/','banana','.jpg'),
-//   new Image('img/','bathroom','.jpg'),
-//   new Image('img/','boots','.jpg'),
-//   new Image('img/','breakfast','.jpg'),
-//   new Image('img/','bubblegum','.jpg'),
-//   new Image('img/','chair','.jpg'),
-//   new Image('img/','cthulhu','.jpg'),
-//   new Image('img/','dog-duck','.jpg'),
-//   new Image('img/','dragon','.jpg'),
-//   new Image('img/','pen','.jpg'),
-//   new Image('img/','pet-sweep','.jpg'),
-//   new Image('img/','scissors','.jpg'),
-//   new Image('img/','shark','.jpg'),
-//   new Image('img/','sweep','.png'),
-//   new Image('img/','tauntaun','.jpg'),
-//   new Image('img/','unicorn','.jpg'),
-//   new Image('img/','usb','.gif'),
-//   new Image('img/','water-can','.jpg'),
-//   new Image('img/','wine-glass','.jpg'),
-// ];
-
 function Image(folder, name, filetype) {
   //this.shownPercent ??
   this.clickCount = 0;
@@ -36,6 +8,10 @@ function Image(folder, name, filetype) {
   this.fileName= folder+name+filetype;
 }
 
+var previous = [];
+var secondToLast = [];
+var current = [];
+var totalClicks = 0;
 
 //**************
 var images = [
@@ -68,9 +44,7 @@ function randomNum() {
   return Math.floor(Math.random()*(images.length));
 }
   //* Using Durstenfeld shuffle algorithm...
-function randomIndex() {
-  return Math.floor(Math.random()*(randomArray.length-3)+3);
-}
+
 
 var randomArray = [];
 function randomArrayGenerator() {
@@ -83,71 +57,69 @@ function randomArrayGenerator() {
 
 var previous = [];
 var secondToLast = [];
+var current = [];
 
 function threeRandomPhotos() {
   randomArrayGenerator();
   secondToLast = previous;
   previous = current;
-  var current = [];
 
   // randomArray.push(current);
   // randomArray.push(previous);
   var nextimage = randomArray.pop();
-  console.log(randomArray.length);
+  // console.log(randomArray.length);
   current.push(nextimage);
 
 
   nextimage = randomArray.pop();
   current.push(nextimage);
-  console.log(randomArray.length);
+  // console.log(randomArray.length);
 
   nextimage = randomArray.pop();
   current.push(nextimage);
-  console.log(randomArray.length);
+  // console.log(randomArray.length);
   randomArray= randomArray.concat(current);
-  console.log(randomArray);
+  // console.log(randomArray);
   return current;
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function imageClicker(event) {
+  totalClicks++;
+  console.log(totalClicks);
+  var img = event.target;
+  var currentIndex = img.getAttribute('Current-Image-Index');
+  current[currentIndex].clickCount++;
+  console.log(current[currentIndex].clickCount);
+}
 
 
 function createImages() {
   var current = threeRandomPhotos();
   var app = document.getElementById('app');
-
+  var img;
   for (var i =0; i<current.length; i++) {
-    var div = document.createElement('div');
-    var img = document.createElement('img');
-    div.setAttribute('class','images');
-    img.setAttribute('src', current[i].fileName);
+
+    img = document.createElement('img');
     img.setAttribute('class','images');
-    div.appendChild(img);
-    app.appendChild(div);
+    console.log(img);
+    img.setAttribute('src', current[i].fileName);
+
+    img.setAttribute('Current-Image-Index', i);
+    current[i].showCount++;
+    console.log(current[i].showCount);
+    console.log(current[i].clickCount);
+    img.addEventListener('click', imageClicker);
+    app.appendChild(img);
   }
 }
-
 createImages();
+
+
+
+
+
 
 // function handleClickCounter(event) {
 //
